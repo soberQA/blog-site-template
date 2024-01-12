4d745939465f7327cf4750e1a420af8bd3985f69
Sikamaru <kathait.kuldeep@outlook.com>
2024-01-12 06:18:53 +0530

    Final commit

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Blog Site Template">
     <meta name="author" content="https://youtube.com/FollowAndrew">    
-    <link rel="shortcut icon" href="images/logo.png"> 
+    <link rel="shortcut icon" href="/wp-content/themes/blog-site-template/assets/images/logo.png"> 
     
     <?php
     wp_head();
@@ -18,7 +18,9 @@
 <body>
     
     <header class="header text-center">	    
-	    <a class="site-title pt-lg-4 mb-0" href="index.html">SiteName.dev</a>
+	    <a class="site-title pt-lg-4 mb-0" href="index.html">
+			<?php echo get_bloginfo('name'); ?>
+		</a>
         
 	    <nav class="navbar navbar-expand-lg navbar-dark" >
            
@@ -27,14 +29,25 @@
 			</button>
 
 			<div id="navigation" class="collapse navbar-collapse flex-column" >
-				<img class="mb-3 mx-auto logo" src="images/logo.png" alt="logo" >			
+			<?php	
+			if(function_exists('the_custom_logo')){
+					
+				$custom_logo_id = get_theme_mod
+				('custom_logo');
+				$logo = wp_get_attachment_image_src
+				($custom_logo_id);
+
+
+				}
+			?>
+				<img class="mb-3 mx-auto logo" src="<?php echo $logo[0] ?>" alt="logo" >			
 				<?php
 				 wp_nav_menu(
 					array(
 						'menu' => 'primary',
 						'container' => '',
 						'theme_location' => 'primary'
-						'items_wrap' => '<ul id="" class="">%3$s</ul>'
+						'items_wrap' => '<ul id=" "class="navbar-nav flex-column text-sm-center text-md-left">%3$s</ul>'
 					)
 
 				);
@@ -42,39 +55,6 @@
 
 				?>
 				
-
-
-
-
-
-
-
-				<ul class="navbar-nav flex-column text-sm-center text-md-left">
-					<li class="nav-item active">
-					    <a class="nav-link" href="index.html"><i class="fas fa-home fa-fw mr-2"></i>Blog Home <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item">
-					    <a class="nav-link" href="post.html"><i class="fas fa-file-alt fa-fw mr-2"></i>Blog Post</a>
-					</li>
-					<li class="nav-item">
-					    <a class="nav-link" href="page.html"><i class="fas fa-file-image fa-fw mr-2"></i>Blog Page</a>
-					</li>
-					<li class="nav-item">
-					    <a class="nav-link" href="archive.html"><i class="fas fa-archive fa-fw mr-2"></i>Blog Archive</a>
-					</li>
-					<li class="nav-item">
-					    <a class="nav-link btn btn-primary" href="contact.html"><i class="fas fa-envelope fa-fw mr-2"></i>Contact Us</a>
-					</li>
-				</ul>
-				<hr>
-				<ul class="social-list list-inline py-3 mx-auto">
-					<li class="list-inline-item"><a href="#"><i class="fab fa-twitter fa-fw"></i></a></li>
-					<li class="list-inline-item"><a href="#"><i class="fab fa-linkedin-in fa-fw"></i></a></li>
-					<li class="list-inline-item"><a href="#"><i class="fab fa-github-alt fa-fw"></i></a></li>
-					<li class="list-inline-item"><a href="#"><i class="fab fa-stack-overflow fa-fw"></i></a></li>
-					<li class="list-inline-item"><a href="#"><i class="fab fa-codepen fa-fw"></i></a></li>
-				</ul>
-
 			</div>
 		</nav>
     </header>
@@ -83,3 +63,5 @@
 	    <header class="page-title theme-bg-light text-center gradient py-5">
 			<h1 class="heading"><?php the_title();?></h1>
 		</header>
+			</body>
+</html>
